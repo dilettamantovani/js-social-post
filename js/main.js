e4fb734a01b4fdeb1e9b15abb2a316f96c681781
dilettits <mantovani.d3@gmail.com>
2023-11-20 09:12:11 +0100

    cosnegna esercizio

--- js/main.js
+++ js/main.js
@@ -63,6 +63,69 @@ clickLike();
 
 function createPosts() {
     posts.forEach(post => {
-        
+        const initials = getInitials(post.author.name)
+    let profileImage = ''
+    if (post.author.image) {
+      profileImage = `<img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">`
+    } else {
+      profileImage = `<div class="profile-pic-default"><span class="profile-pic-default span">${initials}</span></div>`
+    }
+    let postTemplate = `
+      <div class="post" id="${post.id}">
+          <div class="post__header">
+              <div class="post-meta">                    
+                  <div class="post-meta__icon">
+                      ${profileImage}
+                  </div>
+                  <div class="post-meta__data">
+                      <div class="post-meta__author">${post.author.name}</div>
+                      <div class="post-meta__time">${post.created}</div>
+                  </div>                    
+              </div>
+          </div>
+          <div class="post__text">${post.content}</div>
+          <div class="post__image">
+              <img src="${post.media}" alt="">
+          </div>
+          <div class="post__footer">
+              <div class="likes js-likes">
+                  <div class="likes__cta">
+                      <a class="like-button  js-like-button" data-postid="${post.id}">
+                          <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                          <span class="like-button__label">Mi Piace</span>
+                      </a>
+                  </div>
+                  <div class="likes__counter">
+                      Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                  </div>
+              </div> 
+          </div>            
+      </div>`
+    document.getElementById('container').innerHTML += postTemplate
+  })
+  function getInitials (name) {
+    const names = name.split(' ')
+    return names.map(n => n[0]).join('')
+  }
+
+}
+function clickLike () {
+  const likeBtn = document.getElementsByClassName('like-button')
+  for (let i = 0; i < likeBtn.length; i++) {
+    
+    likeBtn[i].addEventListener('click', function (event) {
+      event.preventDefault()
+      const likesCounter = document.getElementById(`like-counter-${i + 1}`)
+      const currentLikes = parseInt(likesCounter.innerText, 10)
+      likeBtn[i].classList.toggle('like-button--liked')
+      if (likeBtn[i].classList.contains('like-button--liked')) {
+        likesCounter.innerText = currentLikes + 1
+        likedposts.push(document.getElementById(i + 1))
+      } else {
+        likesCounter.innerText = currentLikes - 1
+        likedposts.pop(document.getElementById(i + 1))
+      }
+    })
+  }
 }
 
